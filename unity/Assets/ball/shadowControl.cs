9b87fc2ed8ff251e84e87383a45cce8a2dddc7c5
Miguel Barreiro <miguel.barreiro.mail@gmail.com>
2014-11-16 03:39:35 +0000

    added better shadows and 2 walls

--- unity/Assets/ball/shadowControl.cs
+++ unity/Assets/ball/shadowControl.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class shadowControl : MonoBehaviour {
 
 	public Transform ballBody;
+	public Transform floor;
 
-	
-	// Update is called once per frame
+	private float maxHeightDifference;
+
+	public void Awake() {
+		this.maxHeightDifference = ballBody.position.y - floor.position.y;
+	}
+
+
+
 	void Update () {
 		transform.position = new Vector3(ballBody.position.x,transform.position.y,0);
+		float newScale = (ballBody.position.y - floor.position.y) / this.maxHeightDifference + 0.5f;
+		transform.localScale = new Vector3(newScale, 1, 1);
+//		Debug.Log(ballBody.position.y);
+
 	}
 }
