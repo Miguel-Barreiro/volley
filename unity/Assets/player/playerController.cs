8abd1f0d528203e9647ee94df35c2d30e2c605a9
Miguel Barreiro <miguel.barreiro.mail@gmail.com>
2014-11-16 04:20:09 +0000

    added a basic player and basic ui buttons

---
+++ unity/Assets/player/playerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class playerController : MonoBehaviour {
+
+	public float jumpForce = 300.0f;
+
+	// Use this for initialization
+	void Start () {
+		body = (Rigidbody2D)GetComponent<Rigidbody2D>();
+		Debug.Log(body);
+	}
+
+
+	public void jump(){
+		body.AddForce(new Vector2(0.0f, jumpForce));
+	}
+
+	private Rigidbody2D body;
+
+}
