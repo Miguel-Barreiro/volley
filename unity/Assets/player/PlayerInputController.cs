8fbda520b51c730840f00072bb2f23208def2b4b
Miguel-Barreiro <Miguel.Barreiro.mail@gmail.com>
2014-11-18 01:58:46 +0000

    added restart button and now one clicks on the heroes

---
+++ unity/Assets/player/PlayerInputController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(playerController))]
+public class PlayerInputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		_playerController = GetComponent<playerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		_playerController.jump();
+	}
+
+	private playerController _playerController;
+}
