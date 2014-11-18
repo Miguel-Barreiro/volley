7d80406ec8194f57b3dc707492513b6f8e2bfeee
Miguel-Barreiro <Miguel.Barreiro.mail@gmail.com>
2014-11-18 03:11:39 +0000

    added the targets logic and scores

--- unity/Assets/GameLogic/Controller.cs
+++ unity/Assets/GameLogic/Controller.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
+
+	public Text leftScoreLabel;
+	public Text rightScoreLabel;
+
+	public GameObject leftTopTarget;
+	public GameObject leftBotTarget;
+	public GameObject rightTopTarget;
+	public GameObject rightBotTarget;
 
 	// Use this for initialization
 	void Start () {
@@ -10,10 +19,39 @@ public class Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		leftScoreLabel.text = this.scoreLeft.ToString();
+		rightScoreLabel.text = this.scoreRight.ToString();
 	}
 
 	public void Restart(){
 		Application.LoadLevel("GameScene");
 	}
+
+	public void handleTargetCollision(GameObject target){
+		if ( target == leftTopTarget){
+			this.hitTopLeft();
+		}else if ( target == leftBotTarget){
+			this.hitBotLeft();
+		}else if ( target == rightTopTarget){
+			this.hitTopRight();
+		}else if ( target == rightBotTarget){
+			this.hitBotRight();
+		}
+	}
+
+	public void hitTopLeft(){
+		this.scoreLeft += 1;
+	}
+	public void hitBotLeft(){
+		this.scoreLeft += 2;
+	}
+	public void hitTopRight(){
+		this.scoreLeft += 1;
+	}
+	public void hitBotRight(){
+		this.scoreLeft += 2;
+	}
+
+	private int scoreLeft = 0;
+	private int scoreRight = 0;
 }
