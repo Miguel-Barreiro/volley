529abd2943ffe655f4be3f236c1ff8c8180aa076
Miguel-Barreiro <Miguel.Barreiro.mail@gmail.com>
2014-11-22 05:24:36 +0000

    controls for jump and move slowly

--- unity/Assets/player/playerController.cs
+++ unity/Assets/player/playerController.cs
@@ -9,39 +9,52 @@ public class playerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		body = (Rigidbody2D)GetComponent<Rigidbody2D>();
-		target = transform.position;
+		isGrounded = false;
 	}
 
 	public void action( Vector3 targetPosition){
-		target = new Vector2( targetPosition.x, transform.position.y);
+		if ( isGrounded ){
+			target = new Vector2( targetPosition.x, transform.position.y);
+		}
 	}
 
 	void FixedUpdate() {
-		body.MovePosition(Vector3.Lerp(transform.position, target, Time.deltaTime));
+		if (isGrounded)
+			body.MovePosition(Vector3.Lerp(transform.position, target, Time.deltaTime));
 	}
-	
+
+	public void setGrounded(bool grounded){
+		isGrounded = grounded;
+		target = transform.position;
+	}
+
 	void Update() {
-		if(Input.touchCount > 0){
+		if(Input.touchCount > 0 && isGrounded){
 			Vector2 currentPositionScreen = Camera.main.WorldToScreenPoint(transform.position);
 
 			for( int i = 0; i < Input.touchCount; i++){
 				Touch touch = Input.GetTouch(i);
 				if ( (currentPositionScreen.x > Screen.width / 2 && touch.position.x < Screen.width / 2 ) || 
-				    ( currentPositionScreen.x < Screen.width / 2 && touch.position.x < Screen.width/2)){
+				    ( currentPositionScreen.x < Screen.width / 2 && touch.position.x < Screen.width / 2)){
 
 					if ( touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Began ){
 						Vector3 target = Camera.main.ScreenToWorldPoint(touch.position);
 						this.action(target);
 						fingerId = touch.fingerId;
-					}else if( touch.phase == TouchPhase.Ended ){
+					}else if( touch.phase == TouchPhase.Ended && this.isGrounded){
 						this.jump();
+						this.isGrounded = false;
 					}
 				}
 			}
 		}
-
-		if (Input.GetMouseButtonDown(0)) {
-			
+		/*
+		else if (Input.GetMouseButtonDown(0) && this.isGrounded) {
+			this.jump();
+			this.isGrounded = false;
+		}
+		*/
+/*
 			Vector2 currentPositionScreen = Camera.main.WorldToScreenPoint(transform.position);
 			
 			if(( currentPositionScreen.x > Screen.width / 2 && Input.mousePosition.x > Screen.width/2) ||
@@ -49,7 +62,9 @@ public class playerController : MonoBehaviour {
 				Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				this.action(target);
 			}
+
 		}
+*/		 
 
 //		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
 //			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
@@ -62,6 +77,7 @@ public class playerController : MonoBehaviour {
 		body.AddForce(new Vector2(0.0f, jumpForce));
 	}
 
+	private bool isGrounded = false;
 	private int fingerId = -1;
 	private Vector2 target; 
 	private Rigidbody2D body;
